00e0fbb4b518d9d48f89cae411704b906684917a
Nolann Holvoet <nolann.holvoet@epitech.eu>
2023-03-23 02:32:56 -0700

    [ADD] : stylizing input & added sign in button & added create account

--- netfake/components/InputAuth.tsx
+++ netfake/components/InputAuth.tsx
@@ -1,6 +1,9 @@
-const InputAuth = () => {
+interface InputAuthProps {
+    placeholder: string
+}
+const InputAuth = (props: InputAuthProps) => {
   return (
-    <input/>
+    <input placeholder={props.placeholder} className="px-4 py-4 rounded-md bg-[#333]"/>
   )
 }
 
--- netfake/pages/auth.tsx
+++ netfake/pages/auth.tsx
@@ -10,15 +10,23 @@ const Auth = () => {
           </h1>
         </nav>
         <div className="flex justify-center pt-20">
-          <div className="bg-black bg-opacity-70 px-8 py-8 md:py-12 md:px-12 self-center md:w-2/5 max-w-md rounded-md w-4/5">
-            <h2 className="text-white md:text-4xl text-2xl font-semibold pb-8 md:pb-16">
+          <div className="bg-black bg-opacity-70 px-8 py-8 md:pt-16 md:px-16 self-center md:w-2/5 max-w-md rounded-md w-4/5">
+            <h2 className="text-white md:text-4xl text-2xl font-semibold pb-8 md:pb-10">
               Sign in
             </h2>
-            <div className="flex flex-col space-y-5">
-              <InputAuth />
-              <InputAuth />
-              <InputAuth />
+            <div className="flex flex-col space-y-8">
+              <InputAuth placeholder={"Username"}/>
+              <InputAuth placeholder={"Password"}/>
             </div>
+            <button className="bg-red-600 text-white font-semibold py-4 px-4 rounded-md mt-8 w-full">
+              Sign in
+            </button>
+            <div className="flex justify-center md:pt-12 pt-6">
+              <p className="text-white text-base">
+                New to Netfake? 
+                <span className="hover:text-red-600 font-semibold md:pl-4 pl-2  text-sm md:text-base cursor-pointer">Create an accout</span>
+              </p>
+             </div>
           </div>
         </div>
       </div>
