3ea1c55c6c674176e5ae4d9d24d224b16010e9e7
Nolann Holvoet <nolann.holvoet@epitech.eu>
2023-03-23 17:10:46 -0700

    [ADD] : Effect on input for auth

--- netfake/components/InputAuth.tsx
+++ netfake/components/InputAuth.tsx
@@ -3,7 +3,12 @@ interface InputAuthProps {
 }
 const InputAuth = (props: InputAuthProps) => {
   return (
-    <input placeholder={props.placeholder} className="px-4 py-4 bg-[#333] rounded-md"/>
+    <div className="relative">
+    <input id="email" placeholder=" " className="px-6 pt-6 pb-2 bg-[#333] rounded-md block text-white appearance-none focus:outline-none focus:ring-0 peer  w-full "/>
+    <label htmlFor="email" className="absolute text-md text-zinc-400 duration-150 transform -translate-y-3 scale-75 top-4 z-10 origin-[0] left-6 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-3">
+      Email
+    </label>
+    </div>
   )
 }
 
