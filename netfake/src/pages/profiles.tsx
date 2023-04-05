352024e87a12eedc26ce31d83751e985b8a82c58
Nolann Holvoet <nolann.holvoet@epitech.eu>
2023-04-05 16:45:15 -0700

    [ADD] : profiles page added

--- netfake/src/pages/profiles.tsx
+++ netfake/src/pages/profiles.tsx
@@ -1,5 +1,8 @@
 import { NextPageContext } from "next";
 import { getSession } from "next-auth/react";
+import Image from "next/image";
+import useCurrentUser from "@/hooks/useCurrentUser";
+import { useRouter } from "next/router";
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
@@ -17,9 +20,36 @@ export async function getServerSideProps(context: NextPageContext) {
 }
 
 const Profiles = () => {
+  const router = useRouter();
+  const { data: user } = useCurrentUser();
   return (
-    <div>
-      <h1 className="text-2xl text-white">Profiles</h1>
+    <div className="flex items-center h-full justify-center">
+      <div className="flex flex-col">
+        <h1 className="text-3xl md:text-6xl text-white text-center">
+          Who is watching ?
+        </h1>
+        <div className="flex items-center justify-center mt-8">
+          <div
+            onClick={() => {
+              router.push("/");
+            }}
+          >
+            <div className="group flex-row w-44 mx-auto">
+              <div className="w-44 h-44 rounded-md flex items-center justify-center border-2 border-transparent group-hover:cursor-pointer group-hover:border-white overflow-hidden">
+                <Image
+                  src="/images/default-blue.png"
+                  alt="Profiles"
+                  width={200}
+                  height={200}
+                />
+              </div>
+              <div className="mt-4 text-gray-400 text-2xl text-center group-hover:text-white">
+                {user?.name}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
