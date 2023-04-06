8f6f975bff7ecf19959ed68a97614e2e042283e9
Nolann Holvoet <nolann.holvoet@epitech.eu>
2023-04-06 16:25:08 -0700

    [ADD] : Name of the user in AccountModal

--- src/components/AccountMenu.tsx
+++ src/components/AccountMenu.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { signOut } from "next-auth/react";
 import Image from "next/image";
+import useCurrentUser from "@/hooks/useCurrentUser";
 
 interface AccountMenuProps {
   visible: boolean;
 }
 const AccountMenu: React.FC<AccountMenuProps> = ({ visible }) => {
+  const { data } = useCurrentUser();
   if (!visible) {
     return null;
   }
@@ -22,7 +24,7 @@ const AccountMenu: React.FC<AccountMenuProps> = ({ visible }) => {
             className="w-8 rounded-md"
           />
           <p className="text-white text-sm group-hover/item:underline">
-            Username
+            {data?.name}
           </p>
         </div>
         <hr className="bg-gray-600 border-0 h-px my-4" />
